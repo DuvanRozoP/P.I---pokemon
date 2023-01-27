acb507b696414ec67fb00b4f54d54464ce575962
Duvanrozop <rozoparraduvan@gmail.com>
2023-01-27 13:14:05 -0500

    optimized component Landing.jsx and Landing.css

--- client/src/Pages/Landing/Landing.jsx
+++ client/src/Pages/Landing/Landing.jsx
@@ -1,39 +1,36 @@
 import './Landing.css';
-// import wallpaper from '../../assets/14734-pokemon.png';
 
 // & Components
 import Search from '../../Components/Search/Search';
 import Pokemons from '../../Components/Pokemons/Pokemons';
 import Loading from '../../Components/Loading/Loading';
+import Pagination from '../../Components/Pagination/Pagination';
 
 // ~Hooks
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 const Landing = () => {
-  const [state, setState] = useState({ isLoading: true, render: [] });
-
+  const [isLoading, setIsLoading] = useState(true);
+  const [render, setRender] = useState([]);
   const pokemons = useSelector((state) => state.pokemons);
-  const filters = useSelector((state) => state.filters);
-
-  const handlerSlice = (array, start = 0, end = 12) => array.slice(start, end);
 
   useEffect(() => {
-    if (filters?.length > 0)
-      setState({ ...state, isLoading: false, render: handlerSlice(filters) });
-    else if (pokemons.api?.length > 0 || pokemons.db?.length > 0)
-      setState({ isLoading: false, render: handlerSlice(pokemons.api) });
-
+    if (pokemons.api?.length > 0 || pokemons.db?.length > 0) setIsLoading(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pokemons, filters]);
+  }, [pokemons]);
 
-  if (state.isLoading) return <Loading />;
+  const updateRender = (newRender) => {
+    setRender(newRender);
+  };
+
+  if (isLoading) return <Loading />;
 
   return (
     <div className='containerLanding'>
       <Search />
-      <Pokemons arrayPokemons={state.render} />
-      <div>Pagination</div>
+      <Pokemons arrayPokemons={render} />
+      <Pagination setRender={updateRender} />
     </div>
   );
 };
