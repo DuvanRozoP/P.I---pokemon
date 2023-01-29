44433be807670071a1c0c4c748522deba2df1f2b
Duvanrozop <rozoparraduvan@gmail.com>
2023-01-29 18:58:06 -0500

    validation complete for component create

---
+++ client/src/Helpers/validation.js
@@ -0,0 +1,37 @@
+const validateInput = (inputs) => {
+  //const imgs = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png|svg)/;
+  const onlyCaract = /^[a-zA-Z]*$/;
+  const imgsv2 = /^https:\/\//;
+
+  let erros = {};
+
+  const objetoNumbers = {
+    height: inputs.height,
+    weight: inputs.weight,
+    healt: inputs.healt,
+    attack: inputs.attack,
+    defense: inputs.defense,
+    speed: inputs.speed,
+  };
+
+  const isEmpty = (arg) => {
+    for (const iterator in arg)
+      if (arg[iterator].length === 0) erros = { ...erros, [`${iterator}`]: 'Campo obligatorio' };
+  };
+
+  const isNumber = (arg) => {
+    for (const iterator in arg)
+      if (isNaN(arg[iterator])) erros = { ...erros, [`${iterator}`]: 'debe ser numero' };
+  };
+
+  isEmpty(inputs);
+  isNumber(objetoNumbers);
+
+  if (!onlyCaract.test(inputs.name)) erros = { ...erros, name: 'debe contener solo letras' };
+  if (!imgsv2.test(inputs.sprites)) erros = { ...erros, sprites: 'debe ser una Url' };
+
+  console.log('💻 -> validateInput -> erros', erros);
+  return erros;
+};
+
+export default validateInput;
--- client/src/Pages/Create/Create.jsx
+++ client/src/Pages/Create/Create.jsx
@@ -7,11 +7,12 @@ import { useSelector } from 'react-redux';
 // & Component
 import Loading from '../../Components/Loading/Loading';
 
-import { request } from '../../Redux/actions/actions';
+import validateInput from '../../Helpers/validation';
 
 const Create = () => {
   const typesPokemon = useSelector((state) => state.types);
   const [isLoading, setIsLoading] = useState(true);
+
   const [inputs, setInputs] = useState({
     name: '',
     height: '',
@@ -24,46 +25,65 @@ const Create = () => {
     tagTypes: [],
   });
 
+  const [erros, setErros] = useState({
+    name: '',
+    height: '',
+    weight: '',
+    sprites: '',
+    healt: '',
+    attack: '',
+    defense: '',
+    speed: '',
+  });
+
   useEffect(() => {
     if (typesPokemon?.length > 0) setIsLoading(false);
   }, [typesPokemon]);
 
-  const handleChangeInput = (event) => {
+  const handleChangeInput = (event) =>
     setInputs({ ...inputs, [event.target.name]: event.target.value });
-  };
+
+  const handleChangeErros = () => setErros(validateInput(inputs));
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const { name, height, weight, sprites, healt, attack, defense, speed, tagTypes } = inputs;
-    const newPokemon = {
-      name,
-      tagTypes,
-      stats: {
-        healt: Number(healt),
-        attack: Number(attack),
-        defense: Number(defense),
-        speed: Number(speed),
-      },
-      sprites,
-      height: Number(height),
-      weight: Number(weight),
-    };
 
-    const createPokemon = await request.post('/pokemons', newPokemon);
-    alert(createPokemon.data.succes);
+    if (Object.values(erros).length > 1) alert('no debes de tener errores en los campos');
+    else if (inputs.tagTypes.length === 0) alert('debes de tener minimo un tipo para el pokemon');
+    else {
+      console.log('creado correctamente');
+      const { name, height, weight, sprites, healt, attack, defense, speed, tagTypes } = inputs;
+      const newPokemon = {
+        name,
+        tagTypes,
+        stats: {
+          healt: Number(healt),
+          attack: Number(attack),
+          defense: Number(defense),
+          speed: Number(speed),
+        },
+        sprites,
+        height: Number(height),
+        weight: Number(weight),
+      };
+
+      //const createPokemon = await request.post('/pokemons', newPokemon);
+      //alert(createPokemon.data.succes);
+    }
   };
 
   const handleTypes = (event) => {
     const newValue = event.target.value;
-    if (!inputs.tagTypes.includes(newValue)) {
+    if (!inputs.tagTypes.includes(newValue) && inputs.tagTypes.length < 3) {
       setInputs((prevInputs) => {
         return {
           ...prevInputs,
           tagTypes: [...prevInputs.tagTypes, newValue],
         };
       });
+    } else {
+      alert('solo puedes agregar 3 tipos diferentes.');
     }
-    console.log('💻 -> Create -> inputs', inputs);
   };
 
   if (isLoading) return <Loading />;
@@ -74,73 +94,81 @@ const Create = () => {
         <div className='containerPart1'>
           <h1> Crea tu nuevo Pokemon </h1>
 
-          <label>Nombre:</label>
+          <label>Nombre: {erros?.name?.length > 0 ? erros.name : ''}</label>
           <input
             name='name'
             value={inputs.name}
+            onBlur={handleChangeErros}
             onChange={handleChangeInput}
             placeholder='Escribe el nombre.'
             type='text'
           />
 
-          <label>Altura:</label>
+          <label>Altura: {erros?.height?.length > 0 ? erros.height : ''}</label>
           <input
             name='height'
             value={inputs.height}
+            onBlur={handleChangeErros}
             onChange={handleChangeInput}
             placeholder='Escribe el Altura.'
             type='number'
           />
 
-          <label>Peso:</label>
+          <label>Peso: {erros?.weight?.length > 0 ? erros.weight : ''}</label>
           <input
             name='weight'
             value={inputs.weight}
+            onBlur={handleChangeErros}
             onChange={handleChangeInput}
             placeholder='Escribe el Peso.'
             type='number'
           />
 
-          <label>Imagen:</label>
+          <label>Imagen: {erros?.sprites?.length > 0 ? erros.sprites : ''}</label>
           <input
             name='sprites'
             value={inputs.sprites}
+            onBlur={handleChangeErros}
             onChange={handleChangeInput}
             placeholder='Escribe la url de la imagen.'
             type='text'
           />
 
-          <label>Vida:</label>
+          <label>Vida: {erros?.healt?.length > 0 ? erros.healt : ''}</label>
           <input
             name='healt'
             value={inputs.healt}
+            onBlur={handleChangeErros}
             onChange={handleChangeInput}
             placeholder='Escribe la vide del pokemon.'
             type='number'
           />
 
-          <label>Ataque:</label>
+          <label>Ataque: {erros?.attack?.length > 0 ? erros.attack : ''}</label>
           <input
             name='attack'
             value={inputs.attack}
+            onBlur={handleChangeErros}
             onChange={handleChangeInput}
             placeholder='Escribe el ataque del pokemon.'
             type='number'
           />
 
-          <label>Defense:</label>
+          <label>Defense: {erros?.defense?.length > 0 ? erros.defense : ''}</label>
           <input
             name='defense'
             value={inputs.defense}
+            onBlur={handleChangeErros}
             onChange={handleChangeInput}
             placeholder='Escribe la defensa del pokemon.'
             type='number'
           />
 
-          <label>Velocidad:</label>
+          <label>Velocidad: {erros?.speed?.length > 0 ? erros.speed : ''}</label>
           <input
             name='speed'
             value={inputs.speed}
+            onBlur={handleChangeErros}
             onChange={handleChangeInput}
             placeholder='Escribe la velocidad del pokemon.'
             type='number'
